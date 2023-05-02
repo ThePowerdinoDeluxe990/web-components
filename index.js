43bf610ade0192bfcc88ba0ee03f425b77391a1c
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-05-02 10:46:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+class List extends HTMLElement{
+    constructor (){
+        super();
+
+        let shadow= this.attachShadow({mode: 'open'})
+
+        this.divHeader = document.createElement("div")
+        this.divContent = document.createElement("div")
+        this.modePicture=false;
+        shadow.appendChild(this.divHeader)
+        shadow.appendChild(this.divContent)
+    }
+    connectedCallback(){
+            this.divHeader.innerHTML=`<strong>
+            ${this.getAttribute("data-title")}
+            </strong>`
+        let url= this.getAttribute("data-url")
+        let field = this.getAttribute("data-field")
+        if(this.getAttribute("modePicture")){
+            this.modePicture=this.getAttribute("modePicture")
+        }
+        this.divContent.innerHTML=""
+        fetch(url)
+        .then(response=>response.json())
+        .then(cacaculo=>cacaculo.forEach(element => {
+            if(this.modePicture =='true'){
+                this.divContent.innerHTML+=`<img src=${element[field]}>
+                </img>`
+            }else{
+                this.divContent.innerHTML+= `<div>
+                ${element[field]}
+                </div>`
+            }
+           
+        }))
+    }
+}
+
+customElements.define("app-list",List)
